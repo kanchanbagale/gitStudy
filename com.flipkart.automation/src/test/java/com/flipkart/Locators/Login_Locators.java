72c0da6fc984d0e92bd198b5ec99edd26b2e2f96
kanchanbagale <kanchan.bagale@e-zest.in>
2020-10-17 16:40:49 +0530

    merging

--- com.flipkart.automation/src/test/java/com/flipkart/Locators/Login_Locators.java
+++ com.flipkart.automation/src/test/java/com/flipkart/Locators/Login_Locators.java
@@ -12,7 +12,7 @@ public class Login_Locators {
 	public static String login_button4="//button[text()='Logout']";
 	public static String login_button5="//button[text()='Logout']";
 	public static String login_button6="//button[text()='Logout']";
-
+	public static String login_button8="//button[text()='Logout']";
 	
 
 
