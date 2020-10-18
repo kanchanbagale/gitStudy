05fa5aa1064759fc9c5dbca1ba72985289ea5784
kanchanbagale <kanchan.bagale@e-zest.in>
2020-10-18 15:08:07 +0530

    3rd commit from local

--- com.flipkart.automation/src/test/java/com/flipkart/Locators/Login_Locators.java
+++ com.flipkart.automation/src/test/java/com/flipkart/Locators/Login_Locators.java
@@ -8,6 +8,7 @@ public class Login_Locators {
 
 	public static String login_button1="//button[text()='Logout']";
 	public static String login_button2="//button[text()='Logout']";
+	public static String login_button3="//button[text()='Logout']";
 
 
 
