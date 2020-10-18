6c68f26d16e27d4d004dbc166fd4d2f4eaa4f040
kanchanbagale <kanchan.bagale@e-zest.in>
2020-10-18 15:00:16 +0530

    My 1st commmit

--- com.flipkart.automation/src/test/java/com/flipkart/Locators/Login_Locators.java
+++ com.flipkart.automation/src/test/java/com/flipkart/Locators/Login_Locators.java
@@ -7,16 +7,8 @@ public class Login_Locators {
 	public static String login_button="//button[text()='Login']";
 
 	public static String login_button1="//button[text()='Logout']";
-	public static String login_button2="//button[text()='Logout']";
-	public static String login_button3="//button[text()='Logout']";
-	public static String login_button4="//button[text()='Logout']";
-	public static String login_button5="//button[text()='Logout']";
-	public static String login_button6="//button[text()='Logout']";
-	public static String login_button8="//button[text()='Logout']";
-	public static String login_button7="//button[text()='Logout']";
-	public static String login_button9="//button[text()='Logout']";
-	public static String login_button10="//button[text()='Logout']";
-		public static String login_button11="//button[text()='Logout']";
+	
+
 
 
 
